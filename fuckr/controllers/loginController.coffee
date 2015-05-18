8ceb7d3e7f1a67257dd048f4c177c39aa08dc4d9
Some Guy <ts@thomasschneider.com>
2015-05-18 10:10:08 +0200

    Spin when logging in

--- fuckr/controllers/loginController.coffee
+++ fuckr/controllers/loginController.coffee
@@ -1,8 +1,9 @@
 loginController = ($scope, $location, authentication) ->
     $scope.login = ->
+        $scope.logging = true
         authentication.login($scope.email, $scope.password).then(
             -> authentication.authenticate().then -> $location.path('/profiles/')
-            -> $scope.email = $scope.password = ''
+            -> $scope.logging = $scope.email = $scope.password = null
         )
     $scope.tip = ->
         alert("Please sign up using popup window and close it when the 'Create Account' button fades.")
--- fuckr/fuckr.js
+++ fuckr/fuckr.js
@@ -462,12 +462,13 @@
 
   loginController = function($scope, $location, authentication) {
     $scope.login = function() {
+      $scope.logging = true;
       return authentication.login($scope.email, $scope.password).then(function() {
         return authentication.authenticate().then(function() {
           return $location.path('/profiles/');
         });
       }, function() {
-        return $scope.email = $scope.password = '';
+        return $scope.logging = $scope.email = $scope.password = null;
       });
     };
     return $scope.tip = function() {
@@ -551,15 +552,12 @@
   updateProfileController = function($scope, $http, $rootScope, profiles, uploadImage) {
     $scope.profile = {};
     profiles.get($rootScope.profileId).then(function(profile) {
-      alert(angular.toJson(profile));
       return $scope.profile = profile;
     });
     $scope.updateAttribute = function(attribute) {
       var data;
       data = {};
-      alert("updateAttribute(" + attribute + ")");
       data[attribute] = $scope.profile[attribute];
-      alert(angular.toJson(data));
       if (data !== {}) {
         return $http.put('https://primus.grindr.com/2.0/profile', data);
       }
